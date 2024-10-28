e1ca1681942f76b2133f940cd4ff3a0d43709352
mintsuki <mintsuki@protonmail.com>
2024-10-28 21:04:33 +0100

    Update outdated comment

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 #include <limine.h>
 
-// Set the base revision to 2, this is recommended as this is the latest
+// Set the base revision to 3, this is recommended as this is the latest
 // base revision described by the Limine boot protocol specification.
 // See specification for further info.
 
